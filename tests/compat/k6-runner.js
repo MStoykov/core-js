e0e4f93ed6d71f72c4a6ae96e5f3b32ade8c5a1b
Mihail Stoykov <M.Stoikov@gmail.com>
2020-08-10 12:00:06 +0300

    Add a way to test k6

---
+++ tests/compat/k6-runner.js
@@ -0,0 +1,18 @@
+var tests = require('./tests.js').tests;
+var result = {};
+
+for (var key in tests) {
+  var test = tests[key];
+  try {
+    if (typeof test == 'function') {
+      result[key] = !!test();
+    } else result[key] = test.reduce(function (accumulator, $test) {
+      return accumulator && !!$test();
+    }, true);
+  } catch (error) {
+    result[key] = false;
+  }
+}
+
+// eslint-disable-next-line no-console
+console.log(JSON.stringify(result, null, '  '));
